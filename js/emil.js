acda50c12a600a3f133c7433405da0c5586f6a48
Milojensen <154083355+Milojensen@users.noreply.github.com>
2024-05-26 18:47:01 +0200

    tilføjet margin til media sektion hvor det giver mening + javascript

--- js/emil.js
+++ js/emil.js
@@ -5,14 +5,26 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slide");
+    let breadcrumbs = document.getElementsByClassName("breadcrumb");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
+    
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
-}
 
+    for (i = 0; i < breadcrumbs.length; i++) {
+        breadcrumbs[i].className = breadcrumbs[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    breadcrumbs[slideIndex-1].className += " active";
+}
