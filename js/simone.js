1a1f931ef1fe94a81ae147c3140963c0610d64eb
Meladi1 <153490552+Meladi1@users.noreply.github.com>
2024-05-30 13:18:08 +0200

    Rettet småting

--- js/simone.js
+++ js/simone.js
@@ -1,12 +1,11 @@
     const form = document.querySelector('.contact-form');
     const requiredSelectors = ['#name', '#email', '#company'];
 
-    // Function to get the NodeList of required inputs based on the selectors array
+   
     const getRequiredInputs = () => {
         return requiredSelectors.map(selector => form.querySelector(selector));
     };
 
-    // Initialize original placeholders and add event listeners
     const requiredInputs = getRequiredInputs();
     requiredInputs.forEach(input => {
         input.setAttribute('data-original-placeholder', input.placeholder);
@@ -16,14 +15,13 @@
             input.style.backgroundColor = ''; 
             input.style.color = ''; 
     
-            // Restore the original placeholder if error was shown
+            
             if (input.hasAttribute('data-error-shown')) {
                 input.placeholder = input.getAttribute('data-original-placeholder');
                 input.removeAttribute('data-error-shown');
             }
         };
     
-        // Add focus and click event listener
         input.addEventListener('focus', handleFocusOrClick);
         input.addEventListener('click', handleFocusOrClick);
     });
@@ -33,13 +31,13 @@
         const requiredInputs = getRequiredInputs();
        
         requiredInputs.forEach(input => {
-            if (!input.value.trim()) { //Checks if input is empty or only has a space
+            if (!input.value.trim()) { 
                 formIsValid = false;
                 input.classList.add('error');
                 input.style.backgroundColor = 'lightcoral'; 
                 input.style.color = 'white'; 
 
-                // Store the original placeholder if not already stored
+              
                 if (!input.hasAttribute('data-error-shown')) {
                     input.setAttribute('data-original-placeholder', input.placeholder);
                     input.placeholder = 'Skal udfyldes*';
@@ -50,7 +48,7 @@
                 input.style.backgroundColor = ''; 
                 input.style.color = ''; 
 
-                // Restore the original placeholder
+            
                 if (input.hasAttribute('data-error-shown')) {
                     input.placeholder = input.getAttribute('data-original-placeholder');
                     input.removeAttribute('data-error-shown');
@@ -62,13 +60,11 @@
             event.preventDefault();
             return;
         }
-
-        // If form is valid, show custom alert and prevent form submission
+     
         event.preventDefault(); 
         showCustomAlert('Tak for din besked, vi kontakter dig inden for X antal dage');
     });
 
-    // Custom alert functionality
     const customAlertOverlay = document.getElementById('messageSent');
     const customAlertMessage = document.getElementById('customAlertMessage');
     const customAlertButton = document.getElementById('customAlertButton');
